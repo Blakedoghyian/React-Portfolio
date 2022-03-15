96c391fd140e463ace125780b5623e16a8657a17
Blakedoghyian <91994720+Blakedoghyian@users.noreply.github.com>
2022-03-14 23:26:37 -0500

    clean up

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -1,85 +1,4 @@
-// import React from 'react';
-// import { Container, Row } from 'react-bootstrap';
-// import vFriend from '../../assets/images/weather-dashboard.png';
-// import backyardbartender from '../../assets/images/backyardbartender.png';
-// import weatherdashboard from '../../assets/images/weather-dashboard.png';
-
-// import Card from '../Card';
-
-// class Carousel extends React.Component {
-
-//     constructor(props) {
-//         super(props);
-//         this.state = {
-//             items: [
-//                 {
-//                     id: 0,
-//                     title: 'Weather Dashboard',
-//                     subtitle: 'Application for viewing weather conditions by location.',
-//                     imgSrc: 'weatherdashboard',
-//                     link: 'https://github.com/Blakedoghyian/weather-dashboard',
-//                     selected: false
-//                 },
-//                 {
-//                     id: 1,
-//                     title: 'Backyard Bartender',
-//                     subtitle: 'Small scale website for viewing cocktail combinations.',
-//                     imgSrc: 'backyardbartender',
-//                     link: 'https://github.com/jessicalhille/backyard-bartender',
-//                     selected: false
-//                 },
-//                 {
-//                     id: 2,
-//                     title: 'vFriend',
-//                     subtitle: 'Gaming based application for chatting with likeminded individuals.',
-//                     imgSrc: 'vFriend',
-//                     link: 'https://github.com/Arcanaut/vFriend',
-//                     selected: false
-//                 },
-//             ]
-//         }
-//     }
-
-//     handleCardClick = (id, card) => {
-//         let items = [...this.state.items];
-
-//         items[id].selected = items[id].selected ? false : true;
-
-//         items.forEach(item => {
-//             if(item.id !== id) {
-//                 item.selected = false;
-//             }
-//         });
-
-//         this.setState({
-//             items
-//         });
-//     }
-
-//     makeItems = (items) => {
-//         return items.map(item => {
-//             return <Card item={item} onClick={(e => this.handleCardClick(item.id, e))} key={item.id} />
-//         })
-//     }
-
-//     render() {
-//         return(
-//             <Container fluid={true}>
-//                 <Row className="justify-content-around">
-//                     {this.makeItems(this.state.items)}
-//                 </Row>
-//             </Container>
-//         );
-//     }
-// }
-// export default Carousel;
-
-
-// carousel images not loading yet need to figure that out.
-// sunday game plan -> meet mvp
-
 import React from 'react';
-import PhotoList from '../PhotoList';
 import { Carousel, Button } from 'react-bootstrap';
 import weatherdashboard from '../../assets/small/commercial/0.jpg';
 import bartender from '../../assets/small/commercial/1.jpg';
@@ -88,8 +7,8 @@ import budget from '../../assets/small/commercial/3.jpg';
 import employee from '../../assets/small/commercial/4.jpg';
 import quiz from '../../assets/small/commercial/5.jpg';
 
-function Projects(props) {
-    const { currentCategory } = props;
+function Projects() {
+
     return (
         <section>
             <h1>My Projects</h1>
